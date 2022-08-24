1fd698188e5e89ec50b402df8fe3173397dc71bc
JAlmarza98 <j.almarza98@gmail.com>
2022-08-24 18:28:50 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PrivateCloud_Client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
